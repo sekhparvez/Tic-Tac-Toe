8461d8de6d89a2e366b89728f9421f3e2ad0ef88
sekhparvez <sekhparvez@gmail.com>
2022-06-25 15:34:51 -0400

    created board using numpy for 2-d array

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,19 +1,36 @@
-#Steps to complete the Tic Tac Toe Project
+# Steps to complete the Tic Tac Toe Project
 # Step 1 : Create a board using a 2-dimensional array and initialize each element as empty.
+import array
+import numpy as np
+from numpy import ndarray
 
+
+class Board:
+    board = []
+
+    def __init__(self) -> None:
+        print("Board object created")
+
+    def create_board(self, board) -> ndarray:
+        self.board = np.array([["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]])
+        return board
+
+    def print_board(self) -> None:
+        for row in self.board:
+            print(row)
 
 # Step 2: Write a function to check whether the board is filled or not.
 # Iterate over the board and return false if the board contains an empty sign or else return true.
 
 
-#Step 3: Write a function to check whether a player has won or not.
+# Step 3: Write a function to check whether a player has won or not.
 # We have to check all the possibilities that we discussed in the previous section.
 # Check for all the rows, columns, and two diagonals.
 
 
-#Step 4: Write a function to show the board as we will show the board multiple times to the users while they are playing.
+# Step 4: Write a function to show the board as we will show the board multiple times to the users while they are playing.
 
-#Step 5: Write a function to start the game.
+# Step 5: Write a function to start the game.
 # * elect the first turn of the player randomly.
 # * Write an infinite loop that breaks when the game is over (either win or draw).
 # * Show the board to the user to select the spot for the next move.
