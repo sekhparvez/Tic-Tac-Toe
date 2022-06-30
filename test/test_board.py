3edf71a5aed221283c1a2094c2951e54000eb075
sekhparvez <sekhparvez@gmail.com>
2022-06-30 18:00:49 -0400

    updated player turn

--- test/test_board.py
+++ test/test_board.py
@@ -3,16 +3,16 @@ from board import Board
 
 def test_board():
     board = Board()
-    for index, val in enumerate(board.cells):
+    for index, val in enumerate(board.game_board):
 
         if index == 0:
             continue
 
         if index % 2 == 0:
-            board.cells[index] = 'X'
+            board.game_board[index] = 'X'
 
         else:
-            board.cells[index] = 'O'
+            board.game_board[index] = 'O'
 
     print(board)
     print(board.is_board_filled())
